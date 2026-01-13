c44ac773f9db132743f9a0fefc0aaef8f9d998f2
lyl586777 <lyl586777@gmail.com>
2026-01-13 17:59:45 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# TVlive
+![CrimeTV](https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/banner.png)
---
+++ litecon.py
@@ -0,0 +1,83 @@
+ip_version_priority = "ipv4"
+
+source_urls = [
+    "http://tv.850930.xyz/cvd1.m3u", #ADDED BY LEM ON 02/11/2025
+    "https://raw.githubusercontent.com/develop202/migu_video/refs/heads/main/interface.txt" #ADDED BY LEM ON 02/11/2025  
+    
+
+    # "http://tv.850930.xyz/kdsb.txt", #ADDED BY LEM ON 28/09/2024
+    # "http://tv.850930.xyz/kdsb2.txt", #ADDED BY LEM ON 28/09/2024
+    # "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
+    # "https://tanway.oss-cn-beijing.aliyuncs.com/upload/juhe_42636_b9e159834f.txt", #ADDED BY LEM ON 01/06/2025
+    # "http://xg.52sw.top/iptv/litv.php", #ADDED BY LEM ON 31/05/2025
+    # "https://smart.pendy.dpdns.org/m3u/merged_judy.m3u", #ADDED BY LEM ON 31/05/2025
+    # "https://live.wqwqwq.sbs/tv.m3u", #ADDED BY LEM ON 26/11/2024
+    # "https://lu.wqwqwq.sbs/tv.m3u", #ADDED BY LEM ON 26/11/2024
+    # "https://4g.wqwqwq.sbs/iptv.m3u", #ADDED BY LEM ON 26/11/2024
+    # "https://tv.wqwqwq.sbs/iptv.m3u", #ADDED BY LEM ON 26/11/2024
+    # "https://www.mytvsuper.xyz/m3u/Live.m3u", #ADDED BY LEM ON 30/11/2024
+    # "https://live.hacks.tools/tv/ipv4/categories/央视频道.m3u", #ADDED BY LEM ON 10/02/2025
+    # "https://live.hacks.tools/tv/ipv4/categories/地方频道.m3u", #ADDED BY LEM ON 10/02/2025
+    # "https://live.hacks.tools/tv/ipv4/categories/卫视频道.m3u", #ADDED BY LEM ON 10/02/2025
+    # "https://live.hacks.tools/iptv/languages/zho.m3u", #ADDED BY LEM ON 10/02/2025
+    # "https://tv.ccsource.us.kg/live.txt", #ADDED BY LEM ON 08/12/2024
+    # "https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt", #ADDED BY LEM ON 14/11/2024
+    # "https://raw.githubusercontent.com/redrainl/iptv/main/speedtest/zubo_fofa.txt", #ADDED BY LEM ON 01/08/2024
+    # "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    # "https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt", #ADDED BY LEM ON 08/08/2024
+    # "https://raw.githubusercontent.com/n3rddd/MemoryCollection-IPTV/refs/heads/main/itvlist.m3u", #ADDED BY LEM ON 09/11/2024
+    # "https://4gtv.pendy.us.kg/myiptv.m3u", #ADDED BY LEM ON 04/11/2024
+    # "https://aktv.top/live.m3u", #ADDED BY LEM ON 18/10/2024
+    # "https://gitcode.net/ygbh66/test/-/raw/master/oh.txt", #ADDED BY LEM ON 10/09/2024
+    # "https://raw.githubusercontent.com/wwb521/live/main/tv.m3u", #ADDED BY LEM ON 28/09/2024
+    # "https://raw.githubusercontent.com/kakaxi-1/IPTV/main/iptv.txt", #ADDED BY LEM ON 27/09/2024
+    # "https://raw.githubusercontent.com/SingerLan/live/refs/heads/main/jiexi.txt", #ADDED BY LEM ON 20/11/2024
+    # "https://json.doube.eu.org/live/migu/Sub.php", #ADDED BY LEM ON 20/11/2024
+    # "https://www.stream-link.org/stream-link.m3u", #ADDED BY LEM ON 31/08/2024
+    # "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    # "https://raw.githubusercontent.com/joevess/IPTV/main/iptv.m3u8", #ADDED BY LEM ON 08/09/2024  
+    # "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt", #ADDED BY LEM ON 29/07/2024
+    # "http://kxrj.site:55/lib/kx2024.txt", #ADDED BY LEM ON 22/10/2024
+    # "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u", #奥运 ON 29/07/2024
+    # "https://gitlab.com/tvkj/qxitv/-/raw/main/888.txt" #ADDED BY LEM ON 29/07/2024
+
+]
+
+url_blacklist = [
+    "epg.pw/stream/",
+    "103.40.13.71:12390",
+    "[2409:8087:1a01:df::4077]/PLTV/",
+    "8.210.140.75:68",
+    "154.12.50.54",
+    "yinhe.live_hls.zte.com",
+    "8.137.59.151",
+    "[2409:8087:7000:20:1000::22]:6060",
+    "histar.zapi.us.kg",
+    "www.tfiplaytv.vip",
+    "dp.sxtv.top",
+    "111.230.30.193",
+    "148.135.93.213:81",
+    "live.goodiptv.club",
+    "iptv.luas.edu.cn"
+]
+
+announcements = [
+    {
+        "channel": "🤠雷蒙影视直播",
+        "entries": [
+            {"name":"雷蒙影视直播","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/CTVThemeSong1.mp4","logo":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/ICON1.png"},
+            {"name":"CrimeTV LIVE","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/CTVThemeSong2.mp4","logo":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/ICON2.png"},
+            {"name":"更新日期","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/CRIMETVPV1.mkv","logo":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/ICON3.png"},
+            {"name":None,"url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/CRIMETVPV2.mkv","logo":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/ICON4.png"}
+        ]
+    }
+]
+
+epg_urls = [
+    "https://live.fanmingming.com/e.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "http://epg.aptvapp.com/xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
+]
